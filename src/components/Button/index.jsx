b0c9d8520f89851ffe511b1783b3a492acec4b57
Guilherme Vinicius <guilhermev.albuquerq@gmail.com>
2021-06-06 13:21:45 -0300

    context

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,19 @@
+import P from 'prop-types';
+
+export const Button = ({ children, onButtonClick, disabled = false }) => {
+  return (
+    <button
+      disabled={disabled}
+      style={{ fontSize: '30px' }}
+      onClick={onButtonClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: P.node.isRequired,
+  onButtonClick: P.func.isRequired,
+  disabled: P.bool,
+};
